85596c2737ecae8cb30ef96040816930e619e94a
Matt Taylor <8845360+Matttaylor8910@users.noreply.github.com>
2022-12-09 13:45:27 -0700

    2022 day 9

---
+++ src/2022/day09/day09.ts
@@ -0,0 +1,105 @@
+import {readFile} from '../../common/file';
+
+interface Segment {
+  row: number;
+  col: number;
+
+  // the next segment of the rope that follows this segment
+  next?: Segment;
+}
+
+enum Direction {
+  L = 'L',
+  R = 'R',
+  U = 'U',
+  D = 'D',
+}
+
+/**
+ * Return references to the head and tail segments of a length of rope where
+ * each segment has a location in a grid (starting at 0,0) and a reference to
+ * the next segment of the rope
+ * @param length
+ * @returns
+ */
+function getRope(length: number): {head: Segment, tail: Segment} {
+  const head: Segment = {row: 0, col: 0};
+  let tail = head;
+
+  for (let i = 1; i < length; i++) {
+    tail.next = {row: 0, col: 0};
+    tail = tail.next;
+  }
+
+  return {head, tail};
+}
+
+/**
+ * Move a given segment of rope in the given direction, then recursively adjust
+ * the next segment in the rope as needed
+ * @param segment
+ * @param direction
+ */
+function move(segment: Segment, direction: Direction) {
+  if (direction === Direction.L) segment.col--;
+  if (direction === Direction.R) segment.col++;
+  if (direction === Direction.U) segment.row--;
+  if (direction === Direction.D) segment.row++;
+
+  adjustNext(segment);
+}
+
+/**
+ * Given a segement of rope, adjust the next segment (if exists) based on this
+ * segment's location. Then adjust the next segment recursively until we've
+ * reached the tail
+ * @param segment
+ * @returns
+ */
+function adjustNext(segment: Segment) {
+  const head = segment;
+  const tail = segment.next;
+
+  // short circuit if we are at the tail node
+  if (!tail) return;
+
+  // see if the tail is too far away, move it as necessary
+  if (Math.abs(head.col - tail.col) > 1 || Math.abs(head.row - tail.row) > 1) {
+    // both row and col are not the same, diagonal required
+    if (head.col !== tail.col && head.row !== tail.row) {
+      tail.col += head.col > tail.col ? 1 : -1;
+      tail.row += head.row > tail.row ? 1 : -1;
+    }
+    // they're in the same row, move left or right to correct
+    else if (head.col !== tail.col) {
+      tail.col += head.col > tail.col ? 1 : -1;
+    }
+    // they're in the same col, move up or down to correct
+    else {
+      tail.row += head.row > tail.row ? 1 : -1;
+    }
+  }
+
+  adjustNext(tail);
+}
+
+const lines = readFile();
+
+const p1Visited = new Set<string>();
+const p2Visited = new Set<string>();
+const {head: p1Head, tail: p1Tail} = getRope(2);
+const {head: p2Head, tail: p2Tail} = getRope(10);
+
+for (const line of lines) {
+  const [direction, count] = line.split(' ');
+  for (let i = 0; i < Number(count); i++) {
+    move(p1Head, direction as Direction);
+    p1Visited.add(`${p1Tail.row},${p1Tail.col}`);
+
+    move(p2Head, direction as Direction);
+    p2Visited.add(`${p2Tail.row},${p2Tail.col}`);
+  }
+}
+
+console.log(p1Visited.size);
+console.log(p2Visited.size);
