236683f45be1dc33e666b854920fbdb7dc658b90
Matt Taylor <8845360+Matttaylor8910@users.noreply.github.com>
2023-12-13 10:56:41 -0700

    2023 day13, cba to make it cleaner

--- src/2023/day13/day13.ts
+++ src/2023/day13/day13.ts
@@ -7,8 +7,7 @@ function partOne(grids: Grid[]): number {
   let totalAbove = 0;
 
   for (const grid of grids) {
-    const {left, above} = summarizeNotes(grid);
-    // console.log(left, above);
+    const {left, above} = summarizeNotes(grid, false);
     totalLeft += left;
     totalAbove += above;
   }
@@ -16,52 +15,91 @@ function partOne(grids: Grid[]): number {
   return totalLeft + (100 * totalAbove);
 }
 
-function partTwo(lines: string[]): number {
-  return 0;
+function partTwo(grids: Grid[]): number {
+  let totalLeft = 0;
+  let totalAbove = 0;
+
+  for (const grid of grids) {
+    const {left, above} = summarizeNotes(grid, true);
+    totalLeft += left;
+    totalAbove += above;
+  }
+
+  return totalLeft + (100 * totalAbove);
 }
 
-function summarizeNotes(grid: Grid): {left: number, above: number} {
+function summarizeNotes(
+    grid: Grid,
+    allowSmudge: boolean,
+    ): {left: number, above: number} {
+  // look for a horizontal line
   for (let row = 0; row < grid.length - 1; row++) {
     let left = row;
     let right = row + 1;
     let matches = true;
+    let usedSmudge = false;
 
     while (matches && left >= 0 && right < grid.length) {
-      if (grid[left] !== grid[right]) {
-        matches = false;
+      const lr = grid[left];
+      const rr = grid[right];
+      if (lr !== rr) {
+        let useSmudge = false;
+        if (allowSmudge && !usedSmudge) {
+          useSmudge = countDifferences(lr, rr) === 1;
+        }
+        if (useSmudge) {
+          usedSmudge = true;
+        } else {
+          matches = false;
+        }
       }
       left--;
       right++;
     }
 
-    if (matches) {
-      // console.log(`found mirror at row ${row}\n`);
+    if (matches && (usedSmudge || !allowSmudge)) {
       return {left: 0, above: row + 1};
     }
   }
+  // look for a vertical line
   for (let col = 0; col < grid[0].length - 1; col++) {
     let top = col;
     let bottom = col + 1;
     let matches = true;
+    let usedSmudge = false;
 
     while (matches && top >= 0 && bottom < grid[0].length) {
-      // console.log(
-      //     `comparing col: ${getCol(grid, top)} to ${getCol(grid, bottom)}`);
-      if (getCol(grid, top) !== getCol(grid, bottom)) {
-        matches = false;
+      const tc = getCol(grid, top);
+      const bc = getCol(grid, bottom);
+      if (tc !== bc) {
+        let useSmudge = false;
+        if (allowSmudge && !usedSmudge) {
+          useSmudge = countDifferences(tc, bc) === 1;
+        }
+        if (useSmudge) {
+          usedSmudge = true;
+        } else {
+          matches = false;
+        }
       }
       top--;
       bottom++;
     }
 
-    if (matches) {
-      // console.log(`found mirror at col ${col}\n`);
+    if (matches && (usedSmudge || !allowSmudge)) {
       return {left: col + 1, above: 0};
     }
   }
 
-  // console.log(grid.join('\n'));
   throw 'No mirror found';
+}
+
+function countDifferences(str1: string, str2: string) {
+  let count = 0;
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) count++;
+  }
+  return count;
 }
 
 function getCol(grid: Grid, index: number): string {
@@ -76,7 +114,7 @@ function parseInput(): Grid[] {
   let allGrids: Grid[] = [];
   let currentGrid: Grid = [];
 
-  for (const line of readFile('example')) {
+  for (const line of readFile()) {
     if (line.length === 0) {
       allGrids.push(currentGrid);
       currentGrid = [];
@@ -92,7 +130,5 @@ function parseInput(): Grid[] {
   return allGrids;
 }
 
-// 16082 is too low
-
 console.log(partOne(parseInput()));
-// console.log(partTwo(parseInput()));
+console.log(partTwo(parseInput()));
